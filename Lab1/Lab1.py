360b4427651b88412d35dbd79a5c86b664db37b2
AustinS6 <atsilvia1@email.neit.edu>
2025-01-13 11:19:57 -0500

    Lab1 Completion

--- Lab1/Lab1.py
+++ Lab1/Lab1.py
@@ -12,14 +12,43 @@ def difference(people, max_cap):
 def decision(response):
     while response != "y" and response != "n":
         print("***INVALID ENTRY!***")
-        response = input(" Would you like to enter another meeting [y or n]")
+        response = input(" Would you like to enter another meeting [y or n]: ")
 
         return response
 
 
 #--------MAIN EXECUTING CODE----------------------------------
-test = difference(100, 50)
-print(test)
+response = "y"
 
-response = input(print("Would you like to enter another meeting [y or n]"))
-test2 = decision(response)
+while response == "y":
+    meetingName = input("What is the name of the meeting: ")
+    
+    max_cap = int(input("What is the meeting room capacity: "))
+    
+    people = int(input("How many people are attending this meeting: "))
+    
+    diffy = difference(people, max_cap)
+    
+    if diffy > 0:
+        print("Not enough people attending the meeting")
+        
+        print(abs(diffy), "people should be added to the meeting")
+
+        addyadd = int(input("How many people would you like to add to the meeting: "))
+
+        addyadd = diffy + addyadd
+
+    if diffy < 0:
+        print("!MEETING DOES NOT MEET FIRE SAFETY. PLEASE REMOVE PEOPLE FROM MEETING!")
+        
+        print(abs(diffy), "people should be removed from the meeting")
+
+        byebye = int(input("How many people would you like to remove from the meeting: "))
+
+        diffy = diffy + byebye
+        
+    response = input("Would you like to enter another meeting [y or n]: ").lower()
+    
+    test2 = decision(response)
+
+print("Goodbye! :)")
